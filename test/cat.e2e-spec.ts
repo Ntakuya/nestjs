932513923e873d6b946c6eafc08f4d0c17c02215
長濱拓哉 <nagahamatakuya@nagahamatakuyanoMacBook-Pro.local>
2021-01-04 20:07:10 +0900

    e2e(cats) add basic e2e test

---
+++ test/cat.e2e-spec.ts
@@ -0,0 +1,20 @@
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
+import { CatsModule } from './../src/cats/cats.module';
+
+describe('CatsController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [CatsModule],
+    }).compile();
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/cats (GET)', () => {
+    return request(app.getHttpServer()).get('/cats').expect(HttpStatus.OK);
+  });
+});
