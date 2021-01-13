8d2f62d9e354a4ad30382a4778c45110d1a47383
長濱拓哉 <nagahamatakuya@nagahamatakuyanoMacBook-Pro.local>
2021-01-13 23:39:30 +0900

    feat(overview/guards) add basic guards

--- src/controller-for-guard/controller-for-guard.controller.ts
+++ src/controller-for-guard/controller-for-guard.controller.ts
@@ -6,7 +6,12 @@ import {
   Put,
   Param,
   Delete,
+  UseGuards,
+  SetMetadata,
 } from '@nestjs/common';
+import { Roles } from '../core/decorator/role.decorator';
+import { AuthGuard } from '../core/guard/auth.guard';
+import { RoleGuard } from '../core/guard/role.guard';
 import { ControllerForGuardService } from './controller-for-guard.service';
 import { CreateControllerForGuardDto } from './dto/create-controller-for-guard.dto';
 import { UpdateControllerForGuardDto } from './dto/update-controller-for-guard.dto';
@@ -18,16 +23,19 @@ export class ControllerForGuardController {
   ) {}
 
   @Post()
+  @Roles('admin')
   create(@Body() createControllerForGuardDto: CreateControllerForGuardDto) {
     return this.controllerForGuardService.create(createControllerForGuardDto);
   }
 
   @Get()
+  @UseGuards(RoleGuard)
   findAll() {
     return this.controllerForGuardService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard)
   findOne(@Param('id') id: string) {
     return this.controllerForGuardService.findOne(+id);
   }
