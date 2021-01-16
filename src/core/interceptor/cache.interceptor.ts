092de05b911e2860a02df9554be9b869dea3a504
長濱拓哉 <nagahamatakuya@nagahamatakuyanoMacBook-Pro.local>
2021-01-16 19:16:02 +0900

    feat(overview/interceptor) add timeout

---
+++ src/core/interceptor/cache.interceptor.ts
@@ -0,0 +1,18 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, of } from 'rxjs';
+
+@Injectable()
+export class CacheInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const isCache = true;
+    if (isCache) {
+      return of([]);
+    }
+    return next.handle();
+  }
+}
