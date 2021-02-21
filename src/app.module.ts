28eabb2c0488524e29b6f23d330f150c229d9f3f
長濱拓哉 <nagahamatakuya@nagahamatakuyanoMacBook-Pro.local>
2021-02-21 23:10:42 +0900

    feat(base) remove base controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ValidationPipe } from './core/pipes/validation.pipe';
 import { ControllerForGuardModule } from './controller-for-guard/controller-for-guard.module';
 import { LoggintInterceptor } from './core/interceptor/loggint.interceptor';
@@ -27,9 +25,7 @@ import { TodoModule } from './todo/todo.module';
     }),
     TodoModule,
   ],
-  controllers: [AppController],
   providers: [
-    AppService,
     {
       provide: APP_PIPE,
       useClass: ValidationPipe,
