ada8ebbdf745c471a65f48f61a65e00d41d2dbd8
長濱拓哉 <nagahamatakuya@nagahamatakuyanoMacBook-Pro.local>
2021-02-21 23:14:32 +0900

    chore: remove directory

--- src/controller-for-guard/controller-for-guard.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ControllerForGuardService } from './controller-for-guard.service';
-import { ControllerForGuardController } from './controller-for-guard.controller';
-
-@Module({
-  controllers: [ControllerForGuardController],
-  providers: [ControllerForGuardService],
-})
-export class ControllerForGuardModule {}
